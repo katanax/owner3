edd22b6d8fff244af8f52ae91d9717b03a212b03
katanax <masaito1128@gmail.com>
2018-12-12 15:43:30 +0900

    commit 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#![allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    let mut s;
+    {
+        s = String::from("hello");
+        s.push_str(", world!");
+        let s2 = s.clone();
+        //s = String::from("test");
+
+        println!("internal scope: {}", s);
+        println!("internal scope:s2 {}", s2);
+
+    } 
+    println!("external scope: {}", s);
+
+    takes_ownership(s);
+
+    s = give_ownership();
+
+    println!("return fromm give_ownership: {}", s);
+
+    println!("length of string: {}", calc_len(s));
+    
 }
+
+fn calc_len(str: String) -> usize {
+    str.len()
+}
+fn give_ownership() -> String {
+    let s = String::from("Happy code");
+    s
+}
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
